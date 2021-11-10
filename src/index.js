26964cd865db16d1ea310e8508875a66056d95cf
Sebas-jpg <56306846+Sebas-jpg@users.noreply.github.com>
2021-11-10 15:01:07 +0000

    index.js - test changes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const fetch = require('node-fetch');
+
 const xml2js = require('xml2js');
+
 const util = require('./util');
+
 const urls = require('./url');
+
 const constant = require('./constant');
 
 const defaultOptions = {
   protocol: 'https',
   path: '/cas',
   version: constant.CAS_VERSION_3_0,
-  validation_proxy_path: '',
+  validation_proxy_path: ''
 };
 
 class CasClient {
   constructor(endpoint, options = {}) {
+    console.log('cas client log');
     if (util.isEmpty(endpoint)) {
       util.throwError('Missing endpoint');
     }
+
     let version = options.version || defaultOptions.version;
+
     if (!constant.CAS_VERSIONS.includes(version)) {
       util.throwError('Unsupported CAS Version');
     }
@@ -25,9 +32,7 @@ class CasClient {
     this.path = options.path || defaultOptions.path;
     this.protocol = options.protocol || defaultOptions.protocol;
     this.version = options.version || defaultOptions.version;
-    this.validation_proxy_path =
-      options.validation_proxy_path || defaultOptions.validation_proxy_path;
-
+    this.validation_proxy_path = options.validation_proxy_path || defaultOptions.validation_proxy_path;
     this.redirectUrl = util.getCurrentUrl();
   }
 
@@ -37,12 +42,14 @@ class CasClient {
        * Save ticket to sessionStorage if exists
        */
       let ticket = util.getParamFromCurrentUrl('ticket');
+
       if (util.isEmpty(ticket)) {
         let status = util.getParamFromCurrentUrl('status');
+
         if (status === constant.CAS_STATUS_IN_PROCESS) {
           this._handleFailsValdiate(reject, {
             type: constant.CAS_ERROR_AUTH_ERROR,
-            message: 'Missing ticket from return url',
+            message: 'Missing ticket from return url'
           });
         } else {
           window.location.href = urls.getLoginUrl(this, gateway);
@@ -61,125 +68,137 @@ class CasClient {
     return {
       currentUrl: window.location.origin + window.location.pathname,
       currentPath: window.location.pathname,
-      user: user,
+      user: user
     };
   }
 
   _validateTicket(ticket, resolve, reject) {
     let version = this.version;
     let content_type;
+
     switch (version) {
       case constant.CAS_VERSION_2_0:
         content_type = 'text/xml';
         break;
+
       case constant.CAS_VERSION_3_0:
         content_type = 'application/json';
         break;
+
       default:
         throw util.throwError('Unsupported CAS Version');
     }
 
     fetch(urls.getValidateUrl(this, ticket), {
       headers: {
-        'Content-Type': content_type,
-      },
-    })
-      .then(
-        function (response) {
-          response
-            .text()
-            .then(
-              function (text) {
-                switch (version) {
-                  case constant.CAS_VERSION_2_0:
-                    xml2js
-                      .parseStringPromise(text)
-                      .then(
-                        function (result) {
-                          let response = result['cas:serviceResponse'];
-                          if (response['cas:authenticationSuccess']) {
-                            let successes =
-                              response['cas:authenticationSuccess'];
-                            if (successes.length) {
-                              let user = successes[0]['cas:user'][0];
-                              this._handleSuccessValdiate(resolve, user);
-                            }
-                          } else {
-                            let failures =
-                              response['cas:authenticationFailure'];
-                            if (failures.length) {
-                              this._handleFailsValdiate(reject, {
-                                type: constant.CAS_ERROR_AUTH_ERROR,
-                                code: failures[0].$.code.trim(),
-                                message: failures[0]._.trim(),
-                              });
-                            }
-                          }
-                        }.bind(this)
-                      )
-                      .catch(
-                        function (error) {
-                          this._handleFailsValdiate(reject, {
-                            type: constant.CAS_ERROR_PARSE_RESPONSE,
-                            message: 'Failed to parse response',
-                            exception: error,
-                          });
-                        }.bind(this)
-                      );
-                    break;
-                  case constant.CAS_VERSION_3_0:
-                    try {
-                      let json = JSON.parse(text);
-                      if (json.serviceResponse) {
-                        if (json.serviceResponse.authenticationSuccess) {
-                          let user =
-                            json.serviceResponse.authenticationSuccess.user;
-                          this._handleSuccessValdiate(resolve, user);
-                        } else {
-                          this._handleFailsValdiate(reject, {
-                            type: constant.CAS_ERROR_AUTH_ERROR,
-                            code:
-                              json.serviceResponse.authenticationFailure.code,
-                            message:
-                              json.serviceResponse.authenticationFailure
-                                .description,
-                          });
-                        }
-                      }
-                    } catch (error) {
-                      this._handleFailsValdiate(reject, {
-                        type: constant.CAS_ERROR_PARSE_RESPONSE,
-                        message: 'Failed to parse response',
-                        exception: error,
-                      });
+        'Content-Type': content_type
+      }
+    }).then(function (response) {
+      response.text().then(function (text) {
+        switch (version) {
+          case constant.CAS_VERSION_2_0:
+            xml2js.parseStringPromise(text).then(function (result) {
+              let response = result['cas:serviceResponse'];
+
+              if (response['cas:authenticationSuccess']) {
+                let successes = response['cas:authenticationSuccess'];
+
+                if (successes.length) {
+                  let user = successes[0]['cas:user'][0];
+
+                  this._handleSuccessValdiate(resolve, user);
+
+
+                  // console.log('urlparams');
+                  // const urlParams = new URLSearchParams(window.location.search);
+                  // const service = queryParams.get('service');
+                  // const ticket = urlParams.get('ticket');
+                  console.log('ticket' + ticket);
+
+                  var xhttp = new XMLHttpRequest();
+                  xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.response);
+                      xmlRead(this);
                     }
-                    break;
-                  default:
-                    throw util.throwError('Unsupported CAS Version');
+                    else{
+                      console.log(this.response);
+                    }
+                  };
+              
+                  xhttp.open("GET", "https://www.rowa-secure.com/sso/serviceValidate?service=https%3A%2F%2Ffrenchpoteligeo-staging.azurewebsites.net%2Fpoteligeo&ticket=" + ticket, true);
+                                  //    https://www.rowa-secure.com/sso/serviceValidate?service=https%3A%2F%2Ffrenchpoteligeo-staging.azurewebsites.net%2Fpoteligeo&ticket=ST-2927-C2ucqxxxVGEFkfBinjgC-www.rowa-secure.com
+              
+                  xhttp.send();
+                  console.log(xhttp);
+                  xmlRead(xhttp);
                 }
-              }.bind(this)
-            )
-            .catch(
-              function (error) {
-                this._handleFailsValdiate(reject, {
-                  type: constant.CAS_ERROR_PARSE_RESPONSE,
-                  message: 'Failed to parse response',
-                  exception: error,
-                });
-              }.bind(this)
-            );
-        }.bind(this)
-      )
-      .catch(
-        function (error) {
-          this._handleFailsValdiate(reject, {
-            type: constant.CAS_ERROR_FETCH,
-            message: 'Failed to connect CAS server',
-            exception: error,
-          });
-        }.bind(this)
-      );
+              } else {
+                let failures = response['cas:authenticationFailure'];
+
+                if (failures.length) {
+                  this._handleFailsValdiate(reject, {
+                    type: constant.CAS_ERROR_AUTH_ERROR,
+                    code: failures[0].$.code.trim(),
+                    message: failures[0]._.trim()
+                  });
+                }
+              }
+            }.bind(this)).catch(function (error) {
+              this._handleFailsValdiate(reject, {
+                type: constant.CAS_ERROR_PARSE_RESPONSE,
+                message: 'Failed to parse response',
+                exception: error
+              });
+            }.bind(this));
+            break;
+
+          case constant.CAS_VERSION_3_0:
+            try {
+              let json = JSON.parse(text);
+
+              if (json.serviceResponse) {
+                if (json.serviceResponse.authenticationSuccess) {
+                  let user = json.serviceResponse.authenticationSuccess.user;
+
+                  this._handleSuccessValdiate(resolve, user);
+                } else {
+                  this._handleFailsValdiate(reject, {
+                    type: constant.CAS_ERROR_AUTH_ERROR,
+                    code: json.serviceResponse.authenticationFailure.code,
+                    message: json.serviceResponse.authenticationFailure.description
+                  });
+                }
+              }
+            } catch (error) {
+              this._handleFailsValdiate(reject, {
+                type: constant.CAS_ERROR_PARSE_RESPONSE,
+                message: 'Failed to parse response',
+                exception: error
+              });
+            }
+
+            break;
+
+          default:
+            throw util.throwError('Unsupported CAS Version');
+        }
+      }.bind(this)).catch(function (error) {
+        this._handleFailsValdiate(reject, {
+          type: constant.CAS_ERROR_PARSE_RESPONSE,
+          message: 'Failed to parse response',
+          exception: error
+        });
+      }.bind(this));
+    }.bind(this)).catch(function (error) {
+      this._handleFailsValdiate(reject, {
+        type: constant.CAS_ERROR_FETCH,
+        message: 'Failed to connect CAS server',
+        exception: error
+      });
+    }.bind(this));
   }
+  
 
   _handleSuccessValdiate(callback, user) {
     callback(this._getSuccessResponse(user));
@@ -190,7 +209,18 @@ class CasClient {
     error.currentPath = window.location.pathname;
     callback(error);
   }
+
 }
+
+function xmlRead(xml) {
+  var x, i, xmlDoc, txt;
+  xmlDoc = xml.responseXML;
+  txt = "";
+  console.log('function xmlRead')
+  console.log(xmlDoc);
+
+}
+
 
 export default CasClient;
 export { constant };
